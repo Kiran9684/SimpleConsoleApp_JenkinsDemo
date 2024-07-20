b0e85b0f22f3b0eb9c6c4facb2e4a0ceb8fc0eff
Kiran <kirankumarbs1034@gmail.com>
2024-07-20 23:08:19 +0530

    Add project files.

---
+++ SimpleConsoleApp_JenkinsDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleConsoleApp_JenkinsDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
